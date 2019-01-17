62649f40b85e04647b39518900276417fd0dd8bf
debitan <dwm503@gmail.com>
2019-01-17 22:21:18 +0000

    inital commit with just the index page

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express"),
+      app     = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.render("index")
+});
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+    console.log("P. D. Matthews portfolio app has started")
+});
+
